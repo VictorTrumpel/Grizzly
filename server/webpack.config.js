4dc65eacca8599bea69cd61729424bcf8494a21f
victor <>
2021-12-09 18:50:13 +0300

    [*]-eslint,prettier

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,1 +1,30 @@
-const path = require("path");module.exports = {    mode: 'development',    entry: "./src/app.ts",    target: "node",    output: {        filename: "bundle.js",        path: path.resolve(__dirname, "build")    },    module: {        rules: [            {                test: /\.ts?$/,                use: 'ts-loader',                exclude: /node_modules/            }        ]    },    resolve: {        extensions: [".ts", ".js" ]    },}
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  target: 'node',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        enforce: 'pre',
+        use: [
+          'ts-loader',
+          {
+            options: { eslintPath: require.resolve('eslint') },
+            loader: require.resolve('eslint-loader')
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
