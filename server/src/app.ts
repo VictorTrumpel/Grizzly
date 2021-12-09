6241354143337e1bf9d6809b78abb471cf1f4b85
Victor <vtrumpel@xdsoft.ru>
2021-12-09 09:15:38 +0300

    [*]-server-config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,1 @@
-import { helloMessage } from "./test";console.log(helloMessage);
+import { helloMessage } from "./test";const https = require('https')const options = {    hostname: 'localhost',    port: 5000,    path: '',    method: 'GET'}const req = https.request(options, res => {    console.log(`statusCode: ${res.statusCode}`)    res.on('data', d => {        process.stdout.write(d)    })})req.on('error', error => {    console.error(error)})req.end()
