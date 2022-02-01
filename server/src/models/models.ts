dfcaf3151abf1adfabe3e5a29ee653760473a88a
victor <>
2022-02-01 16:23:41 +0300

    [*]-delete statics

--- server/src/models/models.ts
+++ server/src/models/models.ts
@@ -3,22 +3,6 @@ import { ArtistModel } from './ArtistModel';
 import { SongModel } from './SongModel';
 import { DataTypes, Model, Optional } from 'sequelize';
 
-export const Artist = sequelize.define<
-  Model<ArtistModel, Optional<ArtistModel, 'id' | 'role'>>
->('artist', {
-  id: {
-    type: DataTypes.INTEGER,
-    unique: true,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  password: { type: DataTypes.STRING, allowNull: false },
-  nickname: { type: DataTypes.STRING, unique: true, allowNull: false },
-  avatar: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.STRING, defaultValue: 'USER' }
-});
-
 export const Song = sequelize.define<
   Model<SongModel, Optional<SongModel, 'id'>>
 >('song', {
@@ -33,6 +17,22 @@ export const Song = sequelize.define<
   artistName: { type: DataTypes.STRING, allowNull: false },
   artistId: { type: DataTypes.INTEGER, allowNull: false },
   src: { type: DataTypes.STRING, allowNull: false }
+});
+
+export const Artist = sequelize.define<
+  Model<ArtistModel, Optional<ArtistModel, 'id' | 'role'>>
+>('artist', {
+  id: {
+    type: DataTypes.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  nickname: { type: DataTypes.STRING, unique: true, allowNull: false },
+  avatar: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.STRING, defaultValue: 'USER' }
 });
 
 Artist.hasMany(Song);
